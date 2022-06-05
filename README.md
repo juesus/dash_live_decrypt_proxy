7b493884cec522ddd5423020f2a0beb9cea672df
White Red <dannegru40@gmail.com>
2022-06-05 19:23:14 +0300

    now finally a stable functional script that accepts web requests

--- README.md
+++ README.md
@@ -1,20 +1,33 @@
-# WIP
-## Dash Live Stream decrypt & proxy
+# Dash DRM Livestream decrypt API
 
-Script is unstable but functional.
+## DISCLAIMER
 
-First of all make sure ffmpeg is on your PATH
+THERE IS NO WARRANTY FOR THE SCRIPT, IT MIGHT NOT WORK AT ALL AND IT CAN BREAK AND STOP WORKING AT ANY TIME.
 
-Script syntax:
-
-```
-node index [manifest URL] ['["video key", "audio key"]'] [output]
-```
-
-Note: Key/s in json format
-
-Note2: Follow the above keys order
+DULY NOTE THIS IS NOT AN ALL-IN-ONE SOLUTION AND IT REQUIRES YOU TO PROVIDE THE KEYS IN ORDER TO DECRYPT, YOUR DATA MAY VARY AND SO MODIFICATIONS TO THE SCRIPT ARE IN ORDER
 
 ---
 
-Project under MIT license
+## Usage
+
+First of all make sure ffmpeg is on your PATH
+
+By default is listening to port 3000, you can change this behaviour by setting the env var PORT to whatever port you wish
+
+Start:
+
+```
+npm start
+```
+
+### API Routes:
+```
+/?url=[your MPD url]&key_v=[media key(hex) for video]&key_a=[media key(hex) for audio]
+```
+
+### IMPORTANT NOTE:
+At the moment it can only process one stream at a time, more than one will hog up the pipeline
+
+---
+
+&copy; Project under MIT license
--- index.js
+++ index.js
@@ -1,129 +1,241 @@
-var mkfifo = require('mkfifo').mkfifoSync;
-var mpd = require('mpd-parser')
-const fs = require('fs');
-const {default: axios} = require('axios')
-const child = require('child_process')
+import { mkfifo } from "mkfifo";
+import { createWriteStream } from 'fs'
+import { spawn } from 'child_process'
+import { unlink } from "fs/promises";
+import mpd from 'mpd-parser';
+import axios from "axios";
+import { setTimeout } from "timers/promises";
+import express from "express";
+import { v4 } from "uuid";
+const app = express();
 
-if(!fs.existsSync('video')){
-    mkfifo('video', 0600);
-}
-if(!fs.existsSync('audio')){
-    mkfifo('audio', 0600);
-}
+const DEBUG = process.env.DEBUG;
+const PORT = process.env.PORT || 3000;
 
-var prevVideo = '';
-var tempVideo = '';
-var prevAudio = '';
-var tempAudio = '';
-
-function makeid(length) {
-    var result           = '';
-    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    var charactersLength = characters.length;
-    for ( var i = 0; i < length; i++ ) {
-       result += characters.charAt(Math.floor(Math.random() * charactersLength));
+function write(stream, data, cb) {
+    try {
+        if (!stream.write(data)) {
+          v.once('drain', cb);
+        } else {
+          process.nextTick(cb);
+        }
+    } catch (error) {
+        
     }
-    return result;
- }
- function init(callback) {
-    let idV = `concat.${makeid(7)}.txt`
-    let idA = `concat.${makeid(7)}.txt`
-    prevVideo = tempVideo;
-    tempVideo = idV;
-    prevAudio = tempAudio;
-    tempAudio = idA;
-    mkfifo(idV, 0600);
-    mkfifo(idA, 0600);
-    callback({v: idV, a: idA})
 }
 
-function writeStream(audio, video, key, initV, initA){
-
-        init(({v,a}) => {
-            fs.writeFileSync(prevVideo, `ffconcat version 1.0 \nfile 'video' \nfile '${v}'`)
-            child.exec(`ffmpeg -decryption_key ${key[0]} -f mp4 -i <(cat ${initV} <(wget -q -O- ${video})) -preset ultrafast -f hls pipe:1 >> video`)
-            fs.unlinkSync(prevVideo);            
-            fs.writeFileSync(prevAudio, `ffconcat version 1.0 \nfile 'audio' \nfile '${a}'`)
-            child.exec(`ffmpeg -decryption_key ${key[1]} -i <(cat ${initA} <(wget -q -O- ${audio})) -preset ultrafast -f hls pipe:1 >> audio`)
-            fs.unlinkSync(prevAudio);
-        })
+function gethighestBitrate(playlists){
+    let bitrate;
+    let bitIndex;
+    for (let index = 0; index < playlists.length; index++) {
+        bitrate ||= playlists[index].attributes.BANDWIDTH;
+        if(playlists[index].attributes.BANDWIDTH > bitrate){
+            bitrate = playlists[index].attributes.BANDWIDTH;
+            bitIndex = index;
+        }
+    }
+    return bitIndex;
 }
-async function downloadFile(fileUrl, outputLocationPath) {  
-    const writer = fs.createWriteStream(outputLocationPath, {flags: "w"})
-    return axios({
-      method: 'get',
-      url: fileUrl,
-      responseType: 'stream',
-    }).then(response => {
-  
-      //ensure that the user can call `then()` only when the file has
-      //been downloaded entirely.
-  
-      return new Promise((resolve, reject) => {
-        response.data.pipe(writer);
-        let error = null;
-        writer.on('error', err => {
-          error = err;
-          writer.close();
-          reject(err);
-        });
-        writer.on('close', () => {
-          if (!error) {
-            resolve(true);
-          }
-          //no need to call the reject here, as it will have been called in the
-          //'error' stream;
-        });
-      });
-    });
-  }
-(async () => {
-    let mpdURL = process.argv[2];
-    let key = process.argv[3];
-    let output = process.argv[4];
-    let repindex = process.argv[5];
-    let index = 0;
-    
+
+app.get('/', async (req,res) => {
+    if(!req.query.url){
+        res.status(400).send("No URL provided\n")
+        return
+    }
+    if(!req.query.key_v){
+        res.status(400).send("No key for video provided\n")
+        return
+    }
+    if(!req.query.key_a){
+        res.status(400).send("No key for audio provided\n")
+        return
+    }
+    let mpdURL = req.query.url;
     let manifest = await axios.get(mpdURL)
     let parsed = mpd.parse(manifest.data, {manifestUri: `${mpdURL.match("(.*)/(.*)")[1]}/`})
-    let video = parsed.playlists[repindex || 0]
-    index = video.mediaSequence
-    await downloadFile(parsed.mediaGroups.AUDIO.audio[Object.keys(parsed.mediaGroups.AUDIO.audio)[0]].playlists[0].segments[0].map.resolvedUri, parsed.mediaGroups.AUDIO.audio[Object.keys(parsed.mediaGroups.AUDIO.audio)[0]].playlists[0].segments[0].map.uri)
-    await downloadFile(video.segments[0].map.resolvedUri, video.segments[0].map.uri)
-    init(({v,a}) => {
-        // console.log(v);
-        // console.log(a);
-        child.exec(`ffmpeg -y -re -i ${v} -i ${a} -c:v libx264 -tune zerolatency -preset ultrafast -f mpegts pipe:1 > ${output}`, (err, out,stderr) => {
-            // console.log(out);
-            console.log(err);
-            console.log(stderr);
-            // if(stderr){
-            //     fs.unlinkSync(tempVideo);
-            //     fs.unlinkSync(tempAudio);
-            // }
-        })
-        writeStream(
-            `${mpdURL.match("(.*)/(.*)")[1]}/${parsed.mediaGroups.AUDIO.audio[Object.keys(parsed.mediaGroups.AUDIO.audio)[0]].playlists[0].segments[0].map.uri.match("(.*)_(.*)\.(.*)")[1]}_${index}${parsed.mediaGroups.AUDIO.audio[Object.keys(parsed.mediaGroups.AUDIO.audio)[0]].playlists[0].segments[0].uri.match(/\.[0-9a-z]+$/i)[0]}`, 
-            `${mpdURL.match("(.*)/(.*)")[1]}/${video.segments[0].uri.match("(.*)_(.*)\.(.*)")[1]}_${index}${video.segments[0].uri.match(/\.[0-9a-z]+$/i)[0]}`,
-            JSON.parse(key),
-            video.segments[0].map.uri,
-            parsed.mediaGroups.AUDIO.audio[Object.keys(parsed.mediaGroups.AUDIO.audio)[0]].playlists[0].segments[0].map.uri
-        )
-        index++;
-        // console.log(`segment ${index}`);
-        // console.log(`duration ${(video.targetDuration) * 1000}`);
-        setInterval(async () => {
-            writeStream(
-                `${mpdURL.match("(.*)/(.*)")[1]}/${parsed.mediaGroups.AUDIO.audio[Object.keys(parsed.mediaGroups.AUDIO.audio)[0]].playlists[0].segments[0].map.uri.match("(.*)_(.*)\.(.*)")[1]}_${index}${parsed.mediaGroups.AUDIO.audio[Object.keys(parsed.mediaGroups.AUDIO.audio)[0]].playlists[0].segments[0].uri.match(/\.[0-9a-z]+$/i)[0]}`,
-                `${mpdURL.match("(.*)/(.*)")[1]}/${video.segments[0].uri.match("(.*)_(.*)\.(.*)")[1]}_${index}${video.segments[0].uri.match(/\.[0-9a-z]+$/i)[0]}`,
-                JSON.parse(key),
-                video.segments[0].map.uri,
-                parsed.mediaGroups.AUDIO.audio[Object.keys(parsed.mediaGroups.AUDIO.audio)[0]].playlists[0].segments[0].map.uri
-                )
-            index++;
-            // console.log(`segment ${index}`);
-            // console.log(`duration ${(video.targetDuration) * 1000}`);
-        }, (video.targetDuration - 2) * 1000);
-        console.log("working");
+    let stream_play = parsed.playlists[gethighestBitrate(parsed.playlists)]
+    let index_v = stream_play.mediaSequence
+    let index_a = parsed.mediaGroups.AUDIO.audio[Object.keys(parsed.mediaGroups.AUDIO.audio)[0]].playlists[0].segments[0].number;
+    index_v > index_a ? index_v-- : index_a > index_v ? index_a-- : null
+    let vI = await axios({
+        method: 'get',
+        headers:{
+            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:100.0) Gecko/20100101 Firefox/100.0"
+        },
+        url: stream_play.segments[0].map.resolvedUri,
+        responseType: 'arraybuffer',
     })
-})()
+
+    let aI = await axios({
+        method: 'get',
+        headers:{
+            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:100.0) Gecko/20100101 Firefox/100.0"
+        },
+        url: parsed.mediaGroups.AUDIO.audio[Object.keys(parsed.mediaGroups.AUDIO.audio)[0]].playlists[0].segments[0].map.resolvedUri,
+        responseType: 'arraybuffer',
+    })
+
+    let video = v4();
+    let audio = v4();
+    let v, a;
+    let vS_1, aS_1;
+    await new Promise((resolve, reject) => {
+        mkfifo(`/tmp/${video}`, 0o600, async () => {
+            v = createWriteStream(`/tmp/${video}`, {autoClose: false})
+            v.once('ready', () => {
+                DEBUG && console.log("ready video");
+                // vI.data.pipe(v);
+                write(v, vI.data, async () => {
+                    vS_1 = await axios({
+                        method: 'get',
+                        headers:{
+                            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:100.0) Gecko/20100101 Firefox/100.0"
+                        },
+                        url: `${mpdURL.match("(.*)/(.*)")[1]}/${stream_play.segments[0].uri.match("(.*)_(.*)\.(.*)")[1]}_${index_v}${stream_play.segments[0].uri.match(/\.[0-9a-z]+$/i)[0]}`,
+                        responseType: 'stream',
+                    })
+                    DEBUG && console.log("writing video");
+                    DEBUG && console.log(`current segment video ${index_v}`);
+                    vS_1.data.pipe(v, {end: false})
+                    index_v++;
+              })
+
+            })
+            resolve();
+        })
+    })
+    await new Promise((resolve, reject) => {
+        mkfifo(`/tmp/${audio}`, 0o600, async () => {
+            a = createWriteStream(`/tmp/${audio}`, {autoClose: false})
+            a.once('ready', () => {
+                DEBUG && console.log("ready audio");
+                write(a, aI.data, async () => {
+                    aS_1 = await axios({
+                        method: 'get',
+                        headers:{
+                            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:100.0) Gecko/20100101 Firefox/100.0"
+                        },
+                        url: `${mpdURL.match("(.*)/(.*)")[1]}/${parsed.mediaGroups.AUDIO.audio[Object.keys(parsed.mediaGroups.AUDIO.audio)[0]].playlists[0].segments[0].map.uri.match("(.*)_(.*)\.(.*)")[1]}_${index_a}${parsed.mediaGroups.AUDIO.audio[Object.keys(parsed.mediaGroups.AUDIO.audio)[0]].playlists[0].segments[0].uri.match(/\.[0-9a-z]+$/i)[0]}`,
+                        responseType: 'stream',
+                    })
+                    // v.write(vS.data);
+                    DEBUG && console.log("writing audio");
+                    DEBUG && console.log(`current segment audio ${index_a}`);
+                    aS_1.data.pipe(a, {end: false})
+                    index_a++;
+                })
+                // aI.data.pipe(a)
+            })
+            resolve()
+        })
+    })
+    const tA = new AbortController();
+    const tV = new AbortController();
+    let writeToRes = (chunk) =>  res.writable && res.write(chunk)
+    let writeVideoFragment = async () => {
+        let vS = await axios({
+            method: 'get',
+            headers:{
+                "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:100.0) Gecko/20100101 Firefox/100.0"
+            },
+            url: `${mpdURL.match("(.*)/(.*)")[1]}/${stream_play.segments[0].uri.match("(.*)_(.*)\.(.*)")[1]}_${index_v}${stream_play.segments[0].uri.match(/\.[0-9a-z]+$/i)[0]}`,
+            responseType: 'stream',
+        })
+        try {
+            await setTimeout((stream_play.targetDuration * 1000) - 300, undefined, {signal: tV.signal})
+            DEBUG && console.log(`current segment video ${index_v}`);
+            !proc.killed && v.writable && vS.data.pipe(v, {end: false})
+            index_v++;
+        } catch (error) {
+            // console.log(error);
+        }
+    }
+    let writeAudioFragment = async () => {
+        let aS = await axios({
+            method: 'get',
+            headers:{
+                "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:100.0) Gecko/20100101 Firefox/100.0"
+            },
+            url: `${mpdURL.match("(.*)/(.*)")[1]}/${parsed.mediaGroups.AUDIO.audio[Object.keys(parsed.mediaGroups.AUDIO.audio)[0]].playlists[0].segments[0].map.uri.match("(.*)_(.*)\.(.*)")[1]}_${index_a}${parsed.mediaGroups.AUDIO.audio[Object.keys(parsed.mediaGroups.AUDIO.audio)[0]].playlists[0].segments[0].uri.match(/\.[0-9a-z]+$/i)[0]}`,
+            responseType: 'stream',
+        })
+        try {
+            await setTimeout((stream_play.targetDuration * 1000) - 300, undefined, {signal: tA.signal})
+            DEBUG && console.log(`current segment audio ${index_a}`);
+            !proc.killed && a.writable && aS.data.pipe(a, {end: false})
+            index_a++;
+        } catch (error) {
+            // console.log(error);
+        }
+    }
+    let proc = spawn("ffmpeg", [
+        '-y',
+        '-loglevel', 
+        'quiet',
+        '-re',
+        '-decryption_key_v',
+        req.query.key_v,
+        '-vsync',
+        'vfr',
+        '-copyts',
+        '-copytb',
+        '1',
+        '-thread_queue_size',
+        '1024',
+        '-i',
+        `/tmp/${video}`,
+        '-decryption_key_a',
+        req.query.key_a,
+        '-thread_queue_size',
+        '1024',
+        '-i',
+        `/tmp/${audio}`,
+        '-c',
+        'copy',
+        // '-bufsize',
+        // '900k',
+        '-f',
+        'mpegts',
+        'pipe:'
+      ], {stdio: ['ignore', "pipe", 'ignore'], detached: true})
+      v.on('unpipe', writeVideoFragment)
+
+      a.on('unpipe', writeAudioFragment)
+        //   proc.stderr.pipe(process.stderr)
+        proc.stdout.on('data', writeToRes)
+    v.once('error', (err) => {
+        isError = err;
+    })
+    a.once('error', (err) => {
+        isError = err;
+    })
+    req.once('close', () => {
+        aS_1.data.unpipe();
+        vS_1.data.unpipe();
+        v.close();
+        a.close();
+        try {
+            tA.abort();
+            tV.abort();
+        } catch (error) {
+            
+        }
+        unlink(`/tmp/${video}`);
+        unlink(`/tmp/${audio}`);
+        proc.removeListener('data', writeToRes)
+        proc.kill("SIGKILL");
+        a.removeListener('unpipe', writeAudioFragment)
+        v.removeListener('unpipe', writeVideoFragment)
+    })
+})
+
+app.listen(PORT, () => {
+    console.log(`listening on ${PORT}`);
+})
+
+
+
+
+
+
+
